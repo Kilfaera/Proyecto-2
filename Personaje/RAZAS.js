5d7d4be653b6b05d134ada210b7f5f55d692e367
kilfaera <kilfaera@gmail.com>
2022-07-08 22:36:06 -0500

    Me falta solucionar el bug de las batallas y organizar el camino

--- Personaje/RAZAS.js
+++ Personaje/RAZAS.js
@@ -10,3 +10,12 @@ export const mago = new Mago ("", 'Mago', 600, 20, 100, 20, 2);
 export const berserker = new Berserker ("", 'Berserker', 400, 140, 0, 40, 3);
 
 export const slime = new Clase("slime", "monstruo", 700, 70, 0, 20)
+
+const zombie = new Clase("Zombie", "zombie", "700", 50, 0, 20, 100)
+const grifo = new Clase("Grifo", "", 1000, 60, 0, 20, 100)
+const minotauro = new Clase("Minotauro", "", 1200, 40, 0, 30, 100)
+const harpia = new Clase("Harpia", "", 800, 60, 0, 30, 100)
+const ogro = new Clase("Ogro", "", 1300, 70, 0, 40, 100)
+const DIOS = new Clase("DIOS", "DIOS", 10000, 500, 200, 60, 10)
+
+export {zombie, grifo, minotauro, harpia, ogro, DIOS}
